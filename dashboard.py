3a1acc6487564aac065f8ffe65c3d7e7166e25ab
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2023-07-25 22:55:45 +0530

    code refactoring

--- dashboard.py
+++ dashboard.py
@@ -29,8 +29,6 @@ selected = option_menu(
 
 if selected == "Home":
     home.get_home()
-elif selected == "Live pricing":
-    live_pricing.live_price()
 elif selected == "Basic Info":
     basic_info.get_basic_info()
 elif selected == "Prediction":
