272a44a30c194c467853f1a3160ccbb5d5d0db21
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2023-07-26 00:11:55 +0530

    misc

--- bitcoin/bit.py
+++ bitcoin/bit.py
@@ -20,7 +20,7 @@ def get_bit():
     pred_ci = pred.conf_int()
     ax = bitcoin['2018':].plot(label='observed')
     pred.predicted_mean.plot(ax=ax, label='Forecasted', alpha=.2, figsize=(14, 7))
-    pred_uc = resultshigh.get_forecast(steps=30)
+    pred_uc = resultshigh.get_forecast(steps=300)
     pred_ci = pred_uc.conf_int()
     ax = bitcoin.plot(label='observed', color='grey', figsize=(20, 8))
     One_week_values = pred_uc.predicted_mean[:7]
